b0fc2a34dd91979328756141e8802854c6764dc1
Joseph Ogbu <57672199+fallyfox@users.noreply.github.com>
2022-06-30 13:58:59 +0100

    Create assets-and-data.js

---
+++ assets-and-data.js
@@ -0,0 +1,12 @@
+const data = {
+    favourites: [
+        {name:'Pizza 1',id:'1',thumbnail:'https://cdn-icons.flaticon.com/png/512/706/premium/706934.png?token=exp=1656594472~hmac=50a41707028951a7e0281406ca85f151'},
+        {name:'Pizza 2',id:'2',thumbnail:'https://cdn-icons.flaticon.com/png/512/2497/premium/2497913.png?token=exp=1656594472~hmac=7c065c3ac82f4394166025fe725dcb8d'},
+        {name:'Pizza 3',id:'3',thumbnail:'https://www.flaticon.com/free-icons/pizza'},
+        {name:'Pizza 4',id:'4',thumbnail:'https://www.flaticon.com/free-icons/pizza'},
+        {name:'Pizza 5',id:'5',thumbnail:'https://www.flaticon.com/free-icons/pizza'},
+    ],
+    bakersChoice:[
+        {name:'Pizza 5',id:'5',note:'Made with the finest Italian ingredients',thumbnail:'https://www.flaticon.com/free-icons/pizza'}
+    ]
+}
