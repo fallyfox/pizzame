e51cdf59bbb0bcf0d83a0ac5921fce2b53ad9543
Joseph Ogbu <57672199+fallyfox@users.noreply.github.com>
2022-07-04 17:27:28 +0100

    Update assets-and-data.js

--- assets-and-data.js
+++ assets-and-data.js
@@ -1,16 +1,16 @@
 const data = {
     favourites: [
-        {name:'Pizza 1',id:'1',thumbnail:'https://cdn-icons.flaticon.com/png/512/706/premium/706934.png?token=exp=1656594472~hmac=50a41707028951a7e0281406ca85f151'},
-        {name:'Pizza 2',id:'2',thumbnail:'https://cdn-icons.flaticon.com/png/512/2497/premium/2497913.png?token=exp=1656594472~hmac=7c065c3ac82f4394166025fe725dcb8d'},
-        {name:'Pizza 3',id:'3',thumbnail:'https://cdn-icons.flaticon.com/png/512/2454/premium/2454219.png?token=exp=1656948717~hmac=5607fe04b4f853a17565688c2e428308'},
-        {name:'Pizza 4',id:'4',thumbnail:'https://cdn-icons.flaticon.com/png/512/2454/premium/2454220.png?token=exp=1656948717~hmac=728dec1fc357ed61d0d70cd78fdba181'},
-        {name:'Pizza 5',id:'5',thumbnail:'https://cdn-icons.flaticon.com/png/512/3513/premium/3513759.png?token=exp=1656948717~hmac=5c0c411298b143b0dee8cf3c8ddaae4e'}
+        {name:'mozzarella',id:'1',thumbnail:'https://cdn-icons.flaticon.com/png/512/706/premium/706934.png?token=exp=1656594472~hmac=50a41707028951a7e0281406ca85f151'},
+        {name:'parmesan',id:'2',thumbnail:'https://cdn-icons.flaticon.com/png/512/2497/premium/2497913.png?token=exp=1656594472~hmac=7c065c3ac82f4394166025fe725dcb8d'},
+        {name:'provolone',id:'3',thumbnail:'https://cdn-icons.flaticon.com/png/512/2454/premium/2454219.png?token=exp=1656948717~hmac=5607fe04b4f853a17565688c2e428308'},
+        {name:'blue cheese',id:'4',thumbnail:'https://cdn-icons.flaticon.com/png/512/2454/premium/2454220.png?token=exp=1656948717~hmac=728dec1fc357ed61d0d70cd78fdba181'},
+        {name:'broccoli',id:'5',thumbnail:'https://cdn-icons.flaticon.com/png/512/3513/premium/3513759.png?token=exp=1656948717~hmac=5c0c411298b143b0dee8cf3c8ddaae4e'}
     ],
     bakersChoice:[
-        {name:'Pizza 5',id:'5',note:'Made with the finest Italian ingredients',thumbnail:'https://images.pexels.com/photos/1146760/pexels-photo-1146760.jpeg'},
-        {name:'Pizza 5',id:'5',note:'Made with the finest Italian ingredients',thumbnail:'https://images.pexels.com/photos/1653877/pexels-photo-1653877.jpeg'},
-        {name:'Pizza 5',id:'5',note:'Made with the finest Italian ingredients',thumbnail:'https://images.pexels.com/photos/905847/pexels-photo-905847.jpeg'},
-        {name:'Pizza 5',id:'5',note:'Made with the finest Italian ingredients',thumbnail:'https://images.pexels.com/photos/708587/pexels-photo-708587.jpeg'},
-        {name:'Pizza 5',id:'5',note:'Made with the finest Italian ingredients',thumbnail:'https://images.pexels.com/photos/825661/pexels-photo-825661.jpeg'}
+        {name:'pancetta',id:'5',note:'Made with the finest Italian ingredients',thumbnail:'https://images.pexels.com/photos/1146760/pexels-photo-1146760.jpeg'},
+        {name:'speck',id:'5',note:'Made with the finest Italian ingredients',thumbnail:'https://images.pexels.com/photos/1653877/pexels-photo-1653877.jpeg'},
+        {name:'anchovies',id:'5',note:'Made with the finest Italian ingredients',thumbnail:'https://images.pexels.com/photos/905847/pexels-photo-905847.jpeg'},
+        {name:'zucchini',id:'5',note:'Made with the finest Italian ingredients',thumbnail:'https://images.pexels.com/photos/708587/pexels-photo-708587.jpeg'},
+        {name:'pancetta',id:'5',note:'Made with the finest Italian ingredients',thumbnail:'https://images.pexels.com/photos/825661/pexels-photo-825661.jpeg'}
     ]
 }
